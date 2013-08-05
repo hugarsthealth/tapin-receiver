5daf68c081ffc5615c02819c30ec821cb64f184a
Andrew Hughson <andrew@hughson.me>
2013-08-06 11:16:34 +1200

    Fix static analysis problems

--- src/se761/bestgroup/vsmreceiver/DepartmentsActivity.java
+++ src/se761/bestgroup/vsmreceiver/DepartmentsActivity.java
@@ -26,29 +26,29 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-
 public class DepartmentsActivity extends ListActivity {
 
 	private ArrayAdapter<String> adapter;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		// get a list view, give it an adapter and an onclick listener
 		final ListView listView = getListView();
-		adapter = new ArrayAdapter<String>(this,
+		this.adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1);
-		listView.setAdapter(adapter);
+		listView.setAdapter(this.adapter);
 		listView.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
+			@Override
+			public void onItemClick(final AdapterView<?> parent, final View view,
+					final int position, final long id) {
 				// get the name of the department from the item
-				String department = (String) listView
+				final String department = (String) listView
 						.getItemAtPosition(position);
-				
+
 				// give the department to the intent
-				Intent data = new Intent();
+				final Intent data = new Intent();
 				data.putExtra("department", department);
 				// setResult for next activity and finish up
 				setResult(RESULT_OK, data);
@@ -61,28 +61,29 @@ public class DepartmentsActivity extends ListActivity {
 
 	/**
 	 * Task for getting departments from server
+	 * 
 	 * @author Jourdan Harvey, Mike Little
-	 *
+	 * 
 	 */
 	private class HttpTask extends AsyncTask<Void, Void, List<String>> {
 
 		@Override
-		protected List<String> doInBackground(Void... params) {
-			List<String> departments = new ArrayList<String>();
-			HttpGet get = new HttpGet(getStr(R.string.departments_endpoint));
-			HttpClient httpclient = new DefaultHttpClient();
+		protected List<String> doInBackground(final Void... params) {
+			final List<String> departments = new ArrayList<String>();
+			final HttpGet get = new HttpGet(getStr(R.string.departments_endpoint));
+			final HttpClient httpclient = new DefaultHttpClient();
 			try {
 				// get response and build result
-				HttpResponse response = httpclient.execute(get);
-				InputStream ins = response.getEntity().getContent();
-				BufferedReader buff = new BufferedReader(new InputStreamReader(ins));
-				StringBuilder sb = new StringBuilder();
+				final HttpResponse response = httpclient.execute(get);
+				final InputStream ins = response.getEntity().getContent();
+				final BufferedReader buff = new BufferedReader(new InputStreamReader(ins));
+				final StringBuilder sb = new StringBuilder();
 				String line;
 				while ((line = buff.readLine()) != null) {
 					sb.append(line);
 				}
 				// Process the json array
-				JSONArray arr = new JSONArray(sb.toString());
+				final JSONArray arr = new JSONArray(sb.toString());
 				for (int i = 0; i < arr.length(); i++) {
 					Log.v("Departments",
 							arr.getJSONObject(i).getString(
@@ -90,11 +91,11 @@ public class DepartmentsActivity extends ListActivity {
 					departments.add(arr.getJSONObject(i).getString(
 							getStr(R.string.department_name)));
 				}
-			} catch (ClientProtocolException e) {
+			} catch (final ClientProtocolException e) {
 				e.printStackTrace();
-			} catch (IOException e) {
+			} catch (final IOException e) {
 				e.printStackTrace();
-			} catch (JSONException e) {
+			} catch (final JSONException e) {
 				e.printStackTrace();
 			}
 			// return list of departments for onPostExecute
@@ -105,18 +106,20 @@ public class DepartmentsActivity extends ListActivity {
 		 * Add each department in the list to the adapter for the List View
 		 */
 		@Override
-		protected void onPostExecute(List<String> departments) {
-			for (String s : departments) {
-				adapter.add(s);
+		protected void onPostExecute(final List<String> departments) {
+			for (final String s : departments) {
+				DepartmentsActivity.this.adapter.add(s);
 			}
 		}
 	}
+
 	/**
 	 * Helper method for get strings from the values file
+	 * 
 	 * @param id of resource
 	 * @return value of resource requested
 	 */
-	private String getStr(int id) {
+	private String getStr(final int id) {
 		return getResources().getString(id);
 	}
 }
--- src/se761/bestgroup/vsmreceiver/LogActivity.java
+++ src/se761/bestgroup/vsmreceiver/LogActivity.java
@@ -32,36 +32,34 @@ import android.widget.ListView;
 
 public class LogActivity extends Activity {
 
-	@SuppressWarnings("unused")
-	private NfcAdapter mNfcAdapter;
 	private ArrayAdapter<String> listAdapter;
 
 	private BasicClientCookie2 _deptCookie;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_log);
 
-		mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-		listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-		ListView lv = (ListView) findViewById(R.id.logListView);
-		lv.setAdapter(listAdapter);
+		NfcAdapter.getDefaultAdapter(this);
+		this.listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+		final ListView lv = (ListView) findViewById(R.id.logListView);
+		lv.setAdapter(this.listAdapter);
 
-		String savedDepartment = getSharedPreferences("department_cookie", MODE_PRIVATE).getString("department", null);
-		_deptCookie = new BasicClientCookie2("department", savedDepartment != null ? savedDepartment : "Cardiology");
-		_deptCookie.setDomain("vsm.herokuapp.com");
+		final String savedDepartment = getSharedPreferences("department_cookie", MODE_PRIVATE).getString("department", null);
+		this._deptCookie = new BasicClientCookie2("department", savedDepartment != null ? savedDepartment : "Cardiology");
+		this._deptCookie.setDomain("vsm.herokuapp.com");
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(final Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.log, menu);
 		return true;
 	}
 
 	@Override
-	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+	public boolean onMenuItemSelected(final int featureId, final MenuItem item) {
 		if (item.getItemId() == R.id.action_departments) {
 			startActivityForResult(new Intent(this, DepartmentsActivity.class), 1);
 		}
@@ -69,10 +67,10 @@ public class LogActivity extends Activity {
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onOptionsItemSelected(final MenuItem item) {
 		switch (item.getItemId()) {
-		case R.id.action_departments:
-			startActivityForResult(new Intent(this, DepartmentsActivity.class), 1);
+			case R.id.action_departments:
+				startActivityForResult(new Intent(this, DepartmentsActivity.class), 1);
 		}
 		return true;
 	}
@@ -88,42 +86,40 @@ public class LogActivity extends Activity {
 	}
 
 	/**
-	 * Get the department cookie from the shared preferences Set it to the
-	 * cookie field.
+	 * Get the department cookie from the shared preferences Set it to the cookie field.
 	 */
 	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		String dept = data.getStringExtra("department");
-		Editor edit = getSharedPreferences("department_cookie", MODE_PRIVATE).edit();
+	protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+		final String dept = data.getStringExtra("department");
+		final Editor edit = getSharedPreferences("department_cookie", MODE_PRIVATE).edit();
 		edit.putString("department", dept);
 		edit.commit();
-		_deptCookie = new BasicClientCookie2("department", dept.trim());
-		_deptCookie.setDomain("vsm.herokuapp.com");
+		this._deptCookie = new BasicClientCookie2("department", dept.trim());
+		this._deptCookie.setDomain("vsm.herokuapp.com");
 		setTitle(dept);
 	}
 
 	@Override
-	public void onNewIntent(Intent intent) {
+	public void onNewIntent(final Intent intent) {
 		// onResume gets called after this to handle the intent
 		setIntent(intent);
 	}
 
 	/**
-	 * Get any messages from the intent and upload them to the server using the
-	 * SubmitVitalStats task
+	 * Get any messages from the intent and upload them to the server using the SubmitVitalStats task
 	 * 
 	 * @param intent
 	 */
-	void processIntent(Intent intent) {
+	void processIntent(final Intent intent) {
 
-		Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+		final Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
 		// only one message sent during the beam
-		NdefMessage msg = (NdefMessage) rawMsgs[0];
-		String patient = new String(msg.getRecords()[0].getPayload());
+		final NdefMessage msg = (NdefMessage) rawMsgs[0];
+		final String patient = new String(msg.getRecords()[0].getPayload());
 
-		listAdapter.add(patient);
+		this.listAdapter.add(patient);
 		// send the message somewhere
-		SubmitVitalStats vitalStatsUpload = new SubmitVitalStats();
+		final SubmitVitalStats vitalStatsUpload = new SubmitVitalStats();
 		vitalStatsUpload.execute(patient);
 	}
 
@@ -138,15 +134,15 @@ public class LogActivity extends Activity {
 		private DefaultHttpClient httpclient;
 
 		@Override
-		protected Boolean doInBackground(String... params) {
+		protected Boolean doInBackground(final String... params) {
 
 			// instantiates httpclient to make request
-			httpclient = new DefaultHttpClient();
+			this.httpclient = new DefaultHttpClient();
 			// add our department cookie to the client so that we know where to
 			// put our patient
-			httpclient.getCookieStore().addCookie(_deptCookie);
+			this.httpclient.getCookieStore().addCookie(LogActivity.this._deptCookie);
 
-			for (Cookie c : httpclient.getCookieStore().getCookies()) {
+			for (final Cookie c : this.httpclient.getCookieStore().getCookies()) {
 				System.out.println(c);
 			}
 
@@ -154,21 +150,21 @@ public class LogActivity extends Activity {
 			StringEntity patientSE = null;
 			String patientString = null;
 			try {
-				JSONObject j = new JSONObject(params[0]);
+				final JSONObject j = new JSONObject(params[0]);
 				j.getJSONObject("vitalinfo").put("location", "Starship Childrens' Hospital");
 				patientString = j.toString();
 				Log.d("json", patientString);
-			} catch (IndexOutOfBoundsException e) {
+			} catch (final IndexOutOfBoundsException e) {
 				// incorrect msg
 				return false;
-			} catch (JSONException e) {
+			} catch (final JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 
 			try {
 				patientSE = new StringEntity(patientString);
-			} catch (UnsupportedEncodingException e) {
+			} catch (final UnsupportedEncodingException e) {
 				e.printStackTrace();
 				return false;
 			}
@@ -188,7 +184,7 @@ public class LogActivity extends Activity {
 		 * @param httppost
 		 * @return
 		 */
-		private boolean httpPost(StringEntity se, HttpPost httppost) {
+		private boolean httpPost(final StringEntity se, final HttpPost httppost) {
 			// sets the post request as the resulting string
 			httppost.setEntity(se);
 			// sets a request header so the page receiving the request
@@ -198,21 +194,21 @@ public class LogActivity extends Activity {
 
 			try {
 				// execute, get response, read
-				HttpResponse response = httpclient.execute(httppost);
-				InputStream content = response.getEntity().getContent();
-				BufferedReader br = new BufferedReader(new InputStreamReader(content));
+				final HttpResponse response = this.httpclient.execute(httppost);
+				final InputStream content = response.getEntity().getContent();
+				final BufferedReader br = new BufferedReader(new InputStreamReader(content));
 				String line;
-				StringBuilder sb = new StringBuilder();
+				final StringBuilder sb = new StringBuilder();
 				while ((line = br.readLine()) != null) {
 					sb.append(line);
 				}
 				Log.d("response", sb.toString());
 
-			} catch (ClientProtocolException e) {
+			} catch (final ClientProtocolException e) {
 
 				e.printStackTrace();
 				return false;
-			} catch (IOException e) {
+			} catch (final IOException e) {
 				e.printStackTrace();
 				return false;
 			}
